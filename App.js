a08baaf1f6f83ce0988ba5dd76db37a64236d695
Rathina15 <89204229+Rathina15@users.noreply.github.com>
2022-01-11 22:36:34 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import {Login2} from './login2';
+import {Login3} from './login3';
+
+function App() {
+  return (
+  
+   <div className='App'>
+     <center>
+    <Login2 /> 
+    <div >
+   <Login3 />
+    </div>
+   </center>
+   </div>
+   
+  );
+}
+
+export default App;
---
+++ login3.js
@@ -0,0 +1,15 @@
+export function Login3() {
+    return (
+    <div className="user1">
+        
+<div>Login Username </div>
+<h2>USERNAME:</h2> <input type='text' placeholder='Enter your name'></input><br></br>
+     <h2>PASSWORD:</h2> <input type='password' placeholder='Enter the password'></input><br></br>
+     <div>Login button</div>
+<h1><button onclick ="alert('Successfully Logined!')">Login</button><br></br></h1>
+
+
+     
+     </div>);
+     
+}
